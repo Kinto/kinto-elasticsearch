176db56136a4d2714035cd94afeb75599bb3f8b5
Mathieu Leplatre <mathieu@mozilla.com>
2017-05-22 17:34:52 +0200

    Perform operations in bulk (fixes #5)

--- kinto_elasticsearch/listener.py
+++ kinto_elasticsearch/listener.py
@@ -1,6 +1,7 @@
 import logging
 
 import elasticsearch
+from elasticsearch import helpers
 from kinto.core.events import ACTIONS
 
 
@@ -14,20 +15,21 @@ def on_record_changed(event):
     collection_id = event.payload["collection_id"]
     action = event.payload["action"]
 
-    for change in event.impacted_records:
-        try:
-            if action == ACTIONS.DELETE.value:
-                indexer.unindex_record(bucket_id,
-                                       collection_id,
-                                       record=change["old"],
-                                       id_field="id")
-            else:
-                indexer.index_record(bucket_id,
-                                     collection_id,
-                                     record=change["new"],
-                                     id_field="id")
-        except elasticsearch.ElasticsearchException:
-            logger.exception("Failed to index record")
+    try:
+        with indexer.bulk() as bulk:
+            for change in event.impacted_records:
+                if action == ACTIONS.DELETE.value:
+                    bulk.unindex_record(bucket_id,
+                                        collection_id,
+                                        record=change["old"],
+                                        id_field="id")
+                else:
+                    bulk.index_record(bucket_id,
+                                      collection_id,
+                                      record=change["new"],
+                                      id_field="id")
+    except elasticsearch.ElasticsearchException:
+        logger.exception("Failed to index record")
 
 
 def on_server_flushed(event):
